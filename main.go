d474b0c4e7ca2bd255fa40d2dd10ef56ed507533
inosk <inoue.yusuke.928@gmail.com>
2017-02-25 17:39:13 +0900

    コメントを追加

--- main.go
+++ main.go
@@ -6,24 +6,39 @@ import (
   "net/http"
 )
 
+// httpのUpgradeヘッダーでwsをつかうようにする
 var upgrader = websocket.Upgrader{}
 
+// echoするためのハンドラ
 func echo(w http.ResponseWriter, r *http.Request) {
+  // upgrader.Upgradeでhttp -> websocketに変換
+  // webscocketのconnectionオブジェクトを返す
   conn, err := upgrader.Upgrade(w, r, nil)
+
   if err != nil {
     log.Println("upgrade:", err)
   }
 
+  // 関数から抜けるときにコネクションを切断する
   defer conn.Close()
 
+  // 無限ループ
   for {
+    // バッファーからメッセージを読む
     mt, message, err := conn.ReadMessage()
+
+    // readに失敗した場合は、ループを抜ける
     if err != nil {
       log.Println("read:", err)
       break
     }
+
     log.Printf("recv: %s", message)
+
+    // バッファーに書き込む
     err = conn.WriteMessage(mt, message)
+
+    // writeに失敗した場合は、ループを抜ける
     if err != nil {
       log.Println("write:", err)
       break
