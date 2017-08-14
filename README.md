1e7198e4c0ed9738a7df0e4cde0b9f4ffad15264
inosk <inoue.yusuke.928@gmail.com>
2017-08-14 20:01:10 +0900

    グループチャットができるところまで

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # これはなに
 
-go/websocketでchatの実装
+go/websocketでchatの実装  
+一応グループチャットをサポート
 
 # セットアップ
 
@@ -40,6 +41,6 @@ https://github.com/gorilla/websocket/tree/master/examples/chat
   - チャット参加者を表現する構造体
   - readPumpはユーザからの入力をwebsocket経由で受け付けてbroadcat channelに投げる
   - writePumpはwebsocketからのsendを受け付けて画面に表示するためのイベント発行
-- home.html
+- room.html.tmpl
   - フロントエンド
   - onmessageで受信したmessageのdivを作ってbodyに挿入
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "log"
   "net/http"
   "html/template"
+  "strconv"
 
   "goji.io"
   "goji.io/pat"
@@ -12,27 +13,23 @@ import (
 
 var addr = flag.String("addr", ":8080", "http servise address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
+func serveRooms(hubs []*Hub, w http.ResponseWriter, r *http.Request) {
   log.Println(r.URL)
-  if r.URL.Path != "/" {
-    http.Error(w, "Not found", 404)
-    return
-  }
 
-  if r.Method != "GET" {
-    http.Error(w, "Method not allowed", 405)
-    return
-  }
-
-  tmpl, parseErr := template.ParseFiles("home.html.tmpl")
+  tmpl, parseErr := template.ParseFiles("rooms.html.tmpl")
   if parseErr != nil {
     panic(parseErr)
   }
 
+  links := make([]string, len(hubs))
+  for i := range hubs {
+    links[i] = "/rooms/" + strconv.Itoa(i + 1)
+  }
+
   execErr := tmpl.Execute(w, struct {
-    Websocket string
+    Rooms []string
   }{
-    Websocket: "ws",
+    Rooms: links,
   })
 
   if execErr != nil {
@@ -40,16 +37,69 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+/*
+ * htmlはtmplateを使う
+ * goのtemplateは正直イマイチな感じなのでなんかいい感じのテンプレートが欲しい
+ */
+func serveRoom(roomId string, w http.ResponseWriter, r *http.Request) {
+  log.Println(r.URL)
+
+  tmpl, parseErr := template.ParseFiles("room.html.tmpl")
+  if parseErr != nil {
+    panic(parseErr)
+  }
+
+  execErr := tmpl.Execute(w, struct {
+    RoomId string
+    Websocket string
+  }{
+    RoomId: roomId,
+    Websocket: "ws/" + roomId,
+  })
+
+  if execErr != nil {
+    panic(execErr)
+  }
+}
+
+
+/*
+ * 新しいroomを作ってroomのURLにリダイレクト
+ */
+func serveCreateRoom (hubs *[]*Hub, w http.ResponseWriter, r *http.Request) {
+  hub := newHub()
+  go hub.run()
+  *hubs = append(*hubs, hub)
+  roomId := len(*hubs)
+  path := "/rooms/" + strconv.Itoa(roomId)
+  http.Redirect(w, r, path, http.StatusFound)
+}
+
 func main() {
   flag.Parse()
 
-  hub := newHub()
-  go hub.run()
+  // グループごとのhubを管理する配列
+  // 部屋の数だけのサイズ配列ができる
+  hubs := []*Hub{}
 
+  // routing
   mux := goji.NewMux()
-  mux.HandleFunc(pat.Get("/"), serveHome)
-  mux.HandleFunc(pat.Get("/ws"), func(w http.ResponseWriter, r *http.Request) {
+  mux.HandleFunc(pat.Get("/"), func(w http.ResponseWriter, r *http.Request) {
+    serveRooms(hubs, w, r)
+  })
+  mux.HandleFunc(pat.Get("/rooms/new"), func(w http.ResponseWriter, r *http.Request) {
+    // hubsはサーバ全体で共通管理すべきなので参照渡しする
+    serveCreateRoom(&hubs, w, r)
+  })
+  mux.HandleFunc(pat.Get("/ws/:roomId"), func(w http.ResponseWriter, r *http.Request) {
+    roomId, _ := strconv.Atoi(pat.Param(r, "roomId"))
+    hubIndex := roomId - 1
+    hub := hubs[hubIndex]
     serveWs(hub, w, r)
+  })
+  mux.HandleFunc(pat.Get("/rooms/:roomId"), func(w http.ResponseWriter, r *http.Request) {
+    roomId := pat.Param(r, "roomId")
+    serveRoom(roomId, w, r)
   })
 
   err := http.ListenAndServe(*addr, mux)
